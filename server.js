69d2cce2dd2dd1fd790e88b607f29efc63c1fa47
ameza <ang3lm3zafr33azur3@hotmail.com>
2020-04-26 21:30:20 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const compression = require("compression");
+// const compression = require("compression");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
@@ -13,7 +13,7 @@ app
   .then(() => {
     const server = express();
 
-    server.use(compression());
+    // server.use(compression());
 
     server.get("*", (req, res) => {
       return handle(req, res);
